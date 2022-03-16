d2a179ff36d3b2498570b32c9121739a069679d9
Anaye1 <tsigieyigzaw@gmail.com>
2022-03-16 00:39:08 -0500

    First Project Uploaded

---
+++ Tsigie-Project-1/src/com/Day1/Example1.java
@@ -0,0 +1,14 @@
+package com.Day1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("My Name is Tsigie");		
+		System.out.println("My First code in Java");
+		
+	}
+
+}
